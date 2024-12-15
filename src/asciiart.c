acc92b39ae0ac02494e8a915c02d2e9c322e86c0
Joan Domingo Pasarin <jdomingopasarin@icloud.com>
2024-12-15 11:08:49 +0100

    Improve handling of the image edges

--- src/asciiart.c
+++ src/asciiart.c
@@ -1,9 +1,6 @@
 #include <assert.h>
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
@@ -14,13 +11,12 @@
 #define shift(xs, xs_sz) (assert((xs_sz) > 0), (xs_sz)--, *(xs)++)
 
 #define PIXEL_LAYOUT STBIR_1CHANNEL
-#define ASCII_CHAR_SIZE 8
 
-void downscale_img_res(unsigned char *pixels, size_t w, size_t h, size_t comp, float downscale_factor)
+void downscale_img_res(uint8_t *pixels, size_t w, size_t h, size_t comp, float downscale_factor)
 {
     size_t rescaled_w = w/downscale_factor;
     size_t rescaled_h = h/downscale_factor;
-    unsigned char *aux = stbir_resize_uint8_srgb(pixels, w, h, w*comp,
+    uint8_t *aux = stbir_resize_uint8_srgb(pixels, w, h, w*comp,
                                                  NULL, rescaled_w, rescaled_h, rescaled_w*comp,
                                                  PIXEL_LAYOUT);
     stbir_resize_uint8_srgb(aux, rescaled_w, rescaled_h, rescaled_w*comp,
@@ -28,6 +24,8 @@ void downscale_img_res(unsigned char *pixels, size_t w, size_t h, size_t comp, f
                             PIXEL_LAYOUT);
     free(aux);
 }
+
+#define ASCII_CHAR_SIZE 8
 
 typedef enum {
     SPACE,
@@ -58,22 +56,24 @@ const uint8_t ascii_char_pixel_map[ASCII_CHAR_COUNT][ASCII_CHAR_SIZE] = {
     [SQUARE]        = {0x00, 0x7E, 0x7E, 0x7E, 0x7E, 0x7E, 0x7E, 0x00},
 };
 
-Ascii_char grayscale_to_ascii(unsigned char gray_value)
+Ascii_char grayscale_to_ascii(uint8_t gray_value)
 {
     // 0..255 (grayscale value) -> 0..9 (Ascii_char index)
     return gray_value * (ASCII_CHAR_COUNT-1) / 255;
 }
 
-void convert_pixels_to_ascii(unsigned char *pixels, unsigned char *downscaled_pixels, size_t w, size_t h)
+void convert_pixels_to_ascii(uint8_t *pixels, uint8_t *downscaled_pixels, size_t w, size_t h)
 {
     for (size_t y = 0; y < h; y += ASCII_CHAR_SIZE) {
         for (size_t x = 0; x < w; x += ASCII_CHAR_SIZE) {
             size_t i = y*w + x;
             Ascii_char ascii_char = grayscale_to_ascii(downscaled_pixels[i]);
-            uint8_t out_of_bounds = (x+ASCII_CHAR_SIZE < w && y+ASCII_CHAR_SIZE < h) ? 255 : 0;
             for (size_t y_offset = 0; y_offset < ASCII_CHAR_SIZE; y_offset++) {
                 for (size_t x_offset = 0; x_offset < ASCII_CHAR_SIZE; x_offset++) {
-                    pixels[i + x_offset + w*y_offset] = out_of_bounds*((ascii_char_pixel_map[ascii_char][y_offset] >> x_offset) & 1);
+                    if (x+x_offset < w && y+y_offset < h) {
+                        pixels[i + x_offset + w*y_offset] =
+                            255 * ((ascii_char_pixel_map[ascii_char][y_offset] >> x_offset) & 1);
+                    }
                 }
             }
         }
@@ -96,8 +96,8 @@ int main(int argc, char **argv)
     const char *output_path = shift(argv, argc);
 
     size_t width, height;
-    unsigned char *pixels = stbi_load(input_path, (int *)&width, (int *)&height, NULL, 1);
-    unsigned char downscaled_pixels[width*height];
+    uint8_t *pixels = stbi_load(input_path, (int *)&width, (int *)&height, NULL, 1);
+    uint8_t downscaled_pixels[width*height];
     memcpy(downscaled_pixels, pixels, width*height);
     downscale_img_res(downscaled_pixels, width, height, 1, ASCII_CHAR_SIZE);
     convert_pixels_to_ascii(pixels, downscaled_pixels, width, height);
